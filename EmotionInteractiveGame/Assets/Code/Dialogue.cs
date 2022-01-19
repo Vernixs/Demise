72900eb369c7bdb0bc520843e4fd8370c5d1f57a
Vernixs <enecelis2021@elam.co.uk>
2022-01-19 10:40:27 +0000

    bmo dialogue system

--- EmotionInteractiveGame/Assets/Code/Dialogue.cs
+++ EmotionInteractiveGame/Assets/Code/Dialogue.cs
@@ -1,9 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using XNode;
+using TMPro;
 
-[CreateAssetMenu]
-public class Dialogue : NodeGraph { 
-	
+public class Dialogue : MonoBehaviour
+{
+
+    public TextMeshProUGUI textComponent;
+    public string [] lines;
+    public float textSpeed;
+    private int index;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerable TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
 }
