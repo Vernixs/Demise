b97fe5af4816dde685de1fee87f55e0ff8c02f08
Vernixs <enecelis2021@elam.co.uk>
2022-02-02 10:40:21 +0000

    Dialogue

---
+++ EmotionInteractiveGame/Assets/Code/InteractionInputData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "InteractionInputData", menuName = "EmotionInteractiveGame/InteractionInputData")]
+public class InteractionInputData : ScriptableObject 
+{
+    
+
+}
+
+
