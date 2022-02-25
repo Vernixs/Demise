8335b35b06ea6de1964ae3b0b02463877476458a
Vernixs <enecelis2021@elam.co.uk>
2022-02-25 11:43:51 +0000

    fixes

--- EmotionInteractiveGame/Assets/Code/PauseMenu.cs
+++ EmotionInteractiveGame/Assets/Code/PauseMenu.cs
@@ -42,7 +42,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
